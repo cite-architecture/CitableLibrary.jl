17ff74783dc055f0bb9ae4fadc03376915a2aaf2
Neel Smith <dnsmith.neel@gmail.com>
2021-11-24 08:48:53 -0500

    Add make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,1 +1,28 @@
-# Add make file here
+# Run this from repository root, e.g. with
+# 
+#    julia --project=docs/ docs/make.jl
+#
+# Run this from repository root to serve:
+#
+#   julia -e 'using LiveServer; serve(dir="docs/build")'julia -e 'using LiveServer; serve(dir="docs/build")' 
+#
+using Pkg
+Pkg.activate(".")
+Pkg.instantiate()
+
+using Documenter, DocStringExtensions
+using CitableLibrary
+
+makedocs(
+    sitename="CitableLibrary.jl",
+    pages = [
+        "Home" => "index.md",
+        
+       
+     
+    ],
+    )
+
+deploydocs(
+        repo = "github.com/cite-architecture/CitableLibrary.jl.git",
+)
